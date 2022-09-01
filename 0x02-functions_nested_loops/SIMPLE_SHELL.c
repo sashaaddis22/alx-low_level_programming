836595441105c1ffca38ebcbd136cee73a885132
sashaaddis22 <sashaaddis204@gmail.com>
2022-09-01 22:45:47 +0300

    double tasks

---
+++ 0x02-functions_nested_loops/SIMPLE_SHELL.c
@@ -0,0 +1,66 @@
+SIMPLE_SHELL
+#COMMENT
+include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<readline/readline.h>
+#include<readline/history.h>
+  
+#define MAXCOM 1000 // max number of letters to be supported
+#define MAXLIST 100 // max number of commands to be supported
+  
+// Clearing the shell using escape sequences
+#define clear() printf("\033[H\033[J")
+  
+// Greeting shell during startup
+void init_shell()
+{
+    clear();
+    printf("\n\n\n\n******************"
+        "************************");
+    printf("\n\n\n\t****MY SHELL****");
+    printf("\n\n\t-USE AT YOUR OWN RISK-");
+    printf("\n\n\n\n*******************"
+        "***********************");
+    char* username = getenv("USER");
+    printf("\n\n\nUSER is: @%s", username);
+    printf("\n");
+    sleep(1);
+    clear();
+}
+  
+// Function to take input
+int takeInput(char* str)
+{
+    char* buf;
+  
+    buf = readline("\n>>> ");
+    if (strlen(buf) != 0) {
+        add_history(buf);
+        strcpy(str, buf);
+        return 0;
+    } else {
+        return 1;
+    }
+}
+  / Function to print Current Directory.
+void printDir()
+{
+    char cwd[1024];
+    getcwd(cwd, sizeof(cwd));
+    printf("\nDir: %s", cwd);
+}
+  
+// Function where the system command is executed
+void execArgs(char** parsed)
+{
+    // Forking a child
+    pid_t pid = fork(); 
+  
+    if (pid == -1) {
+        printf("\nFailed forking child..");
+        return;
+    
